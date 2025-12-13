26d940d80ca4356c17699563bc0f7abaf34601f1
Ajit Kumar <ajitkumar22@gmail.com>
2025-12-13 20:58:13 +0530

    landing page ready

--- components/common/footer.tsx
+++ components/common/footer.tsx
@@ -1,7 +1,9 @@
 export default function Footer() {
     return (
-        <footer>
-            IBuiltThis
+        <footer className="bg-muted/50 border-t py-12">
+            <div className="wrapper px-12">
+                Footer Comes here
+            </div>
         </footer>
     );
 }
--- components/common/header.tsx
+++ components/common/header.tsx
@@ -1,7 +1,68 @@
-export default function Header() {
+import { CompassIcon, HomeIcon, SparklesIcon, UserIcon } from "lucide-react";
+import Link from "next/link";
+import { Button } from "../ui/button";
+
+const Logo = () => {
     return (
-        <header>
-            IBuiltThis
+        <Link href="/" className="flex items-center gap-2 group">
+            <div className="size-8 rounded-lg bg-primary flex items-center justify-center">
+                <SparklesIcon className="size-4 text-primary-foreground" />
+            </div>
+            <span className="text-xl font-bold">I<span className="text-primary">Built</span>This</span>
+
+        </Link >
+    );
+}
+
+
+export default function Header() {
+    const isSignedIn = true;
+    return (
+        <header className="sticky top-0 z-50 bg-background/50 backdrop-blur-sm border-b
+        supports-backdrop-blur:bg-background/60 ">
+            <div className="wrapper px-12">
+                <div className="flex h-16 items-center justify-between">
+                    <Logo />
+                    <nav className="flex items-center gap-1">
+                        <Link href="/"
+                            className="flex items-center gap-2 px-3 py-2 text-sm font-medium text-muted-foreground hover:text-foreground transition-colors hover:bg-muted/50 rounded-md"
+                        >
+                            <HomeIcon className="size-4" />
+                            <span>Home</span>
+                        </Link>
+
+                        <Link href="/explore"
+                            className="flex items-center gap-2 px-3 py-2 text-sm font-medium text-muted-foreground hover:text-foreground transition-colors hover:bg-muted/50 rounded-md"
+                        >
+                            <CompassIcon className="size-4" />
+                            <span>Explore</span>
+                        </Link>
+                    </nav>
+                    <div className="flex items-center gap-3">
+                        {isSignedIn ? (
+                            <>
+                                <Button asChild>
+                                    <Link href="/submit">
+                                        <SparklesIcon className="size-4" />
+                                        <span>Submit Project</span>
+                                    </Link>
+                                </Button>
+                                {/* Clerk User */}
+                                <Button variant="ghost">
+                                    <UserIcon className="size-4" />
+                                </Button>
+                            </>
+                        ) : (
+                            <>
+                                <Button variant="ghost"> Sign In </Button>
+                                <Button> Sign Up </Button>
+                            </>
+                        )}
+                    </div>
+
+
+                </div>
+            </div>
         </header>
     );
 }
